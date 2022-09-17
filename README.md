74f58a4dd37d645c2aca64b08b8a4a620bc839a3
Julia Filippova <julia.94@bk.ru>
2022-09-17 15:58:07 +0300

    feat: start project

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# zoo
+# «Zoo»
+* Верска по макету [Figma](https://www.figma.com/file/KQCnifSfIXHCiDxzem3q7f/online-zoo-3-weeks) 
+* Верстка адаптивная, минимальный размер экрана 320px
+* Используется сборщик проектов Gulp
+* Используется препроцессор SCSS
+* Используется расширение [PerfectPixel для Google Chrome](https://chrome.google.com/webstore/detail/perfectpixel-by-welldonec/dkaagdgjmgdmbnecmcefdhjekcoceebi?hl=en)
+* Весь дополнительный функционал написан на чистом JavaScript
---
+++ gulpfile.js
@@ -0,0 +1,83 @@
+const { src, dest, watch, parallel, series } = require("gulp");
+const scss = require("gulp-sass")(require("sass"));
+const concat = require("gulp-concat");
+const uglify = require("gulp-uglify-es").default;
+const autoprefixer = require("gulp-autoprefixer");
+const imagemin = require("gulp-imagemin");
+const del = require("del");
+const sync = require("browser-sync").create();
+
+function browserSync() {
+  sync.init({
+    server: {
+      baseDir: "src/",
+    },
+  });
+}
+
+function styles() {
+  return src("src/scss/style.scss")
+    .pipe(scss({ outputStyle: "compressed" }))
+    .pipe(concat("style.min.css"))
+    .pipe(
+      autoprefixer({
+        overrideBrowserslist: ["last 10 version"],
+        grid: true,
+      })
+    )
+    .pipe(dest("src/css"))
+    .pipe(sync.stream());
+}
+
+function cleanDist() {
+  return del("dist");
+}
+
+function images() {
+  return src("src/img/**/*")
+    .pipe(
+      imagemin([
+        imagemin.gifsicle({ interlaced: true }),
+        imagemin.mozjpeg({ quality: 75, progressive: true }),
+        imagemin.optipng({ optimizationLevel: 5 }),
+        imagemin.svgo({
+          plugins: [{ removeViewBox: true }, { cleanupIDs: false }],
+        }),
+      ])
+    )
+    .pipe(dest("dist/img"));
+}
+
+function scripts() {
+  return src(["node_modules/jquery/dist/jquery.js", "src/js/main.js"])
+    .pipe(concat("main.min.js"))
+    .pipe(uglify())
+    .pipe(dest("src/js"))
+    .pipe(sync.stream());
+}
+
+function build() {
+  return src(
+    [
+      "src/css/style.min.css",
+      "src/fonts/**/*",
+      "src/js/main.min.js",
+      "src/*.html",
+    ],
+    { base: "app" }
+  ).pipe(dest("dist"));
+}
+
+function watching() {
+  watch(["src/scss/**/*.scss"], styles);
+  watch(["src/js/**/*.js", "!src/js/main.min.js"], scripts);
+  watch(["src/*.html"]).on("change", sync.reload);
+}
+exports.styles = styles;
+exports.scripts = scripts;
+exports.images = images;
+exports.watching = watching;
+exports.cleanDist = cleanDist;
+
+exports.build = series(cleanDist, images, build);
+exports.default = parallel(styles, scripts, browserSync, watching);
