02d8288fcfb4d86c2ddb43df45564ec845803049
Julia Filippova <julia.94@bk.ru>
2022-10-13 22:57:25 +0300

    feat: add range slider

--- src/ts/utils.ts
+++ src/ts/utils.ts
@@ -10,14 +10,39 @@ const menuBurger = () => {
         header.classList.toggle("active-header");
     })
 }
-menuBurger();
-const validation = () => {
-    const v = document.querySelector(".donate-info__currency") as HTMLInputElement;
-    if (v) {
-        v.oninput = () => {
-            v.value = v.value.replace(/[^0-9\.]/g, "");
-        };
-    }
 
+menuBurger();
+
+const range = () => {
+    const rangeDots = document.querySelectorAll('.donate-info__range-line li') as unknown as HTMLLIElement[];
+    const circle = document.querySelector("#active-range-line") as HTMLDivElement;
+    const donateSum = document.querySelectorAll(".donate-info__range-sum li") as unknown as HTMLLIElement[];
+    const inputValue = document.querySelector('.donate-info__currency') as HTMLInputElement;
+
+    const clickValue = (array : HTMLLIElement[]) => {
+        array.forEach((dot, i) => {
+            dot.onclick = () => {
+                circle.className = '';
+                const visiable = () => {
+                    circle.style.display = "block";
+                }
+                circle.classList.add(`transforms-${i}`);
+                donateSum.forEach(d => d.classList.remove('active-range-text'));
+                donateSum[i].classList.add('active-range-text');
+                inputValue.value = donateSum[i].innerHTML + '.00';
+                setTimeout(visiable, 50);
+            }
+        });
+        inputValue.oninput = () => {
+            inputValue.value = inputValue.value.replace(/[^0-9\.]/g, "");
+            circle.style.display = "none";
+            circle.className = '';
+            donateSum.forEach(d => d.classList.remove('active-range-text'));
+        }
+    }
+    if (rangeDots.length || donateSum.length) {
+        clickValue(rangeDots);
+        clickValue(donateSum);
+    }
 }
-validation();
+range();
